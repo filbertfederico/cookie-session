835e71a2d97bbbca59b0f9dc2ede9637ca91f5d3
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2023-05-15 22:03:05 +0700

    Update index.js import css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CookiesProvider } from "react-cookie";
 import App from "./App";
+import "./index.css"
 
 ReactDOM.render(
    <CookiesProvider>
